05b7858c7cd13f27a500aab5cef0ba94d81ce411
arckhr21 <arcad.khrulev@gmail.com>
2022-01-10 08:21:42 +0300

    current changes of  month

--- src/calendar.js
+++ src/calendar.js
@@ -23,6 +23,8 @@ const calendar = () => {
         console.log(calendar);
         console.log(ci);
 
+        if (!ci[0]) {  
+
         //выводим элемент calendar_item на страницу и добавляем в него текст
         let calendDays = [];
         
@@ -32,7 +34,7 @@ const calendar = () => {
         let curMonth = anyMonth,
             //curMonth = date.getMonth(),
             //curMonth = 1,
-            curDayMonth = date.getDate(),
+           // curDayMonth = date.getDate(),
             curYear = date.getFullYear();
         
         // console.log(curMonth);
@@ -58,12 +60,13 @@ const calendar = () => {
         }
         console.log(allNumDays);
 
-        let indCal = (+allNumDays-1) +8; //вводим переменную для работы циклв вывода календаря, вместо 38 (7 + 31) - indCal (индекс календаря)
+        let indCal = (+allNumDays-1) +8; //вводим переменную для работы цикла вывода календаря, вместо 38 (7 + 31) - indCal (индекс календаря)
         console.log(indCal);
         
-        if (!ci[0]) {             // проверка наличия динамической верстки
+        //создание и вывод таблицы календаря
+       // if (!ci[0]) {             // проверка наличия динамической верстки календаря. Если её нет, то создаём календарь.
 
-            console.log(ci[0]);
+           // console.log(ci[0]);
 
             for (let i=0; i<=(indCal+inDayWeek); i++) {
 
@@ -84,26 +87,108 @@ const calendar = () => {
 
                 calendar.appendChild(calendDays[i]);
 
-            }
+            } //конец цикла for
         
-        } // end if - проверка наличия динамической верстки
+        } else { //если календарь был создан, удаляем его и вновь выводим.
+
+            let elChild = document.querySelectorAll('.calendar_item');
+            console.log('дочерние элементы ' + elChild);
+            //for (let j=0; j<=38; j++) {
+            for (let j=0; j<=44; j++) {    
+                if (elChild[j]) {
+                    calendar.removeChild(elChild[j]);
+                }
+            }
+
+            //========================================================================================
+            //расчет первого дня месяца и др. 
+            //========================================================================================
+             //выводим элемент calendar_item на страницу и добавляем в него текст
+                let calendDays = [];
+                
+                //получаем текущие значения дня, месяца и года
+                let date = new Date();
+
+                let curMonth = anyMonth,
+                    //curMonth = date.getMonth(),
+                    //curMonth = 1,
+                // curDayMonth = date.getDate(),
+                    curYear = date.getFullYear();
+                
+                // console.log(curMonth);
+                // console.log(curDayMonth);
+                // console.log(curYear);
+                // console.log(allMonths[curMonth]);
+
+                //определяем на какой день недели приходится 1-ое число месяца
+                let dateFDW = new Date(curYear, curMonth, 1),
+                    dayWeek = dateFDW.getDay();
+                console.log('первый день месяца ' + daysWeek[dayWeek -1]);
+                let inDayWeek = dayWeek -1; //индекс для  дня недели первого дня месяца
+
+                //определяем число дней в месяце в зависимости от високосного или обычного года
+                let allNumDays = 0;
+
+                if (!curYear%4) { //проверяем високосный год, или нет
+                    allNumDays = daysMonthLY[curMonth];  //год високосный
+                    //allNumDays = daysMonthLY[1];
+                } else {
+                    allNumDays = daysMonthSY[curMonth];
+                    //allNumDays = daysMonthSY[1];
+                }
+                console.log(allNumDays);
+
+                let indCal = (+allNumDays-1) +8; //вводим переменную для работы цикла вывода календаря, вместо 38 (7 + 31) - indCal (индекс календаря)
+                console.log(indCal);
+                // =======================================================================================================
+ 
+            for (let i=0; i<=(indCal+inDayWeek); i++) {
+                console.log('проверка родителя ' + calendar);
+              
+                calendDays[i] = document.createElement('div');
+                calendDays[i].classList.add('calendar_item');
+                
+                if(i>0 && i<=7) {
+                    calendDays[i].innerHTML = daysWeek[i-1];
+                } else {
+                
+                if (i<(8+inDayWeek)) {
+                        calendDays[i].innerHTML = "";
+                    } else {
+                        calendDays[i].innerHTML = numDaysMonth[i-(8+inDayWeek)];
+                    }
+
+                };
+
+                calendar.appendChild(calendDays[i]);
+
+            } //конец цикла for
+        // }
+            
+         } // end if - проверки наличия динамической верстки
     
     } // end function
 
-    //выбор месяцв с поле <select></select>
+    //выбор месяца в поле <select>
     function selectMonth(numMonth) { 
-        let calendarItem = document.querySelectorAll('.calendar_item')[0],
+    //function selectMonth() { 
+        let calendarItemSelect= document.querySelectorAll('.calendar_item')[0], //переменная ячейки календаря, куда потом будет помещён тег <select>
             sMonth = document.createElement('select'),
             optionMonth = [],
-            sm = document.querySelectorAll('.select_month')[0];
+            // sm = [];
+            // sm = document.querySelectorAll('.select_month');
+            sm = document.querySelector('.select_month'); //переменная поля <select>
+
         
-        if (!sm) {
+        //формируем вёрстку поля выбора месяца <select>
+        //if (!sm[0]) {  //исключаем появление второго поля <select>
+        if (!sm) {  //исключаем появление второго поля <select>
                
             sMonth.classList.add('select_month');
-            calendarItem.appendChild(sMonth); //добавили в первую строку тег <select></select>
+            calendarItemSelect.appendChild(sMonth); //добавили в первую ячейку календаря тег <select>
 
             for (let j=0; j<=11; j++) {
-                optionMonth[j] = document.createElement('option'); //создаем элемент <option></option>
+                optionMonth[j] = document.createElement('option'); //создаем элемент <option>
                 optionMonth[j].classList.add('option_month'); 
                 //console.log(optionMonth[j]);
         
@@ -113,16 +198,17 @@ const calendar = () => {
 
                 sMonth.appendChild(optionMonth[j]);
 
-                sMonth.selectedIndex = numMonth;
+                // sMonth.selectedIndex = n;
+                // console.log(n);
             }
-        }
-    }
+        } //end <select> and <option>
+    } //end function selectMonth
 
-    n = 1;
-    numMonth = 1;
-    console.log('номер выбранного месяца ' + n);
+   
+
+    //n=9;
     outCalendar(n);
-    selectMonth(numMonth);
+    selectMonth(n);
 
 };
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import './assets/scss/index.scss';
 //import about from './about';
 import calendar from './calendar';
 
+//window.addEventListener('DOMContentLoaded', function(){
 
-//console.log("Hello World!");
+    //console.log("Hello World!");
 
-//about();
-calendar();
+    //about();
+    calendar();
+
+//})
+
 
 
 
